d49bdfb7d3775a10a0c532ebb0bb8a09e272dc8e
Neerav Garg <neeravgarg366@gmail.com>
2023-01-03 19:03:28 +0530

    ui change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
       <Contact />
-      <Footer />
+      <SocialLinks />
     </div>
   );
 }
